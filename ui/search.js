62c305ed75bc12d4431a5d6e9392de7c6f183bc8
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-07-11 16:04:41 +0530

    Properly show and hide searchbar

--- ui/search.js
+++ ui/search.js
@@ -3,25 +3,30 @@
 
 $(function() {
     // Show and hide search bar
-    $(".search-button").on("click", function() {
+    function showSearchBar() {
         $("body").addClass("search-focus");
         // Use a timeout to add focus to avoid double animation in firefox
         setTimeout(function() {
             $(".search-entry").focus().data("search-ready", true);
         }, 300);
-    });
+    }
+
+    function hideSearchBar() {
+        $("body").removeClass("search-focus");
+        $(".search-entry").data("search-ready", false);
+    }
+
+    $(".search-button").on("click", showSearchBar);
 
     $(document).on("click", function(e) {
-        if (e.target !== $(".search-button")[0] && e.target !== $(".search-entry")[0] && $(".search-entry").data("search-ready")) {
-            $("body").removeClass("search-focus");
-            $(".search-entry").data("search-ready", false);
+        if (!$(e.target).closest(".search-entry").length && $(".search-entry").data("search-ready")) {
+            hideSearchBar();
         }
     });
 
-
     $(".search-entry").keypress(function(e) {
-        if(e.which == 13){
-            $("body").removeClass("search-focus");
+        if (e.which === 13) {
+            hideSearchBar();
             e.preventDefault();
             libsb.emit('navigate', {view: "meta", mode: "search", tab: "search-local", query: $(".search-entry").val()});
         }
